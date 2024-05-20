d0b01162aab6ade129d6302b26a860bc8acba85d
Ilham <muhammadilhamsjn@gmail.com>
2024-05-20 12:50:44 +0700

    cluster message and bug fix

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 import express from "express";
-
+import "dotenv/config";
 import apiRouter from "./routes/api.js";
 import fs from "fs";
 import { initializePuppeteer } from "./componen/sendMessage.js";
 import rateLimit from "express-rate-limit";
+import { initializeCluster } from "./componen/cluster.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // limit all request
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 100, // Limit each IP to 100 requests
-//   message: {
-//     status: "error",
-//     message: "Too many requests, please try again later.",
-//   },
-// });
-// app.use(limiter);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests
+  message: {
+    status: "error",
+    message: "Too many requests, please try again later.",
+  },
+});
+app.use(limiter);
 
-initializePuppeteer();
-// initializeCluster();
+// initializePuppeteer();
+initializeCluster();
 // Route to send message
 app.use("/", apiRouter);
 // Fungsi untuk mengirim pesan menggunakan Puppeteer
--- componen/cluster.js
+++ componen/cluster.js
@@ -1,60 +1,59 @@
 import { Cluster } from "puppeteer-cluster";
-import fs from "fs";
-import puppeteer from "puppeteer";
 
 let cluster;
 
 export const initializeCluster = async () => {
-  if (!cluster) {
-    cluster = await Cluster.launch({
-      concurrency: Cluster.CONCURRENCY_PAGE,
-      maxConcurrency: 1,
-      monitor: true,
-      puppeteerOptions: {
-        headless: false,
-        defaultViewport: null,
-        userDataDir: "./userData",
-      },
-    });
+  cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_PAGE,
+    maxConcurrency: 1,
+    monitor: true,
+    puppeteerOptions: {
+      headless: true,
+      defaultViewport: false,
+      userDataDir: "./userData",
+    },
+  });
 
-    cluster.on("taskerror", (err, data, willRetry) => {
-      if (willRetry) {
-        console.warn(
-          `Encountered an error while crawling ${data}. ${err.message}\nThis job will be retried`
-        );
-      } else {
-        console.error(`Failed to crawl ${data}: ${err.message}`);
-      }
-    });
+  cluster.on("taskerror", (err, data, willRetry) => {
+    if (willRetry) {
+      console.warn(
+        `Encountered an error while crawling ${data}. ${err.message}\nThis job will be retried`
+      );
+    } else {
+      console.error(`Failed to crawl ${data}: ${err.message}`);
+    }
+  });
 
-    cluster.task(async ({ page, data: url }) => {
-      try {
-        await page.goto(url);
+  await cluster.task(async ({ page, data: url }) => {
+    try {
+      await page.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+      );
+      await page.goto(url);
 
-        // await page.waitForSelector(
-        //   "#app > div > span:nth-child(3) > div > span > div > div > div > div"
-        // );
-        // await page.waitForSelector(
-        //   "#app > div > span:nth-child(3) > div > span > div > div > div > div > div"
-        // );
+      await page.waitForSelector(
+        "#app > div > span:nth-child(3) > div > span > div > div > div > div"
+      );
+      await page.waitForSelector(
+        "#app > div > span:nth-child(3) > div > span > div > div > div > div > div"
+      );
 
-        await page.waitForSelector("._ak1r");
+      await page.waitForSelector("._ak1r");
 
-        await new Promise((resolve) => setTimeout(resolve, 2000)); // Wait 2 seconds
+      await new Promise((resolve) => setTimeout(resolve, 2000)); // Wait 2 seconds
 
-        await page.keyboard.press("Enter");
+      await page.keyboard.press("Enter");
 
-        await new Promise((resolve) => setTimeout(resolve, 2000)); // Wait 2 seconds
+      await new Promise((resolve) => setTimeout(resolve, 2000)); // Wait 2 seconds
 
-        return {
-          status: "success",
-          message: `Message sent to ${nomor} successfully`,
-        };
-      } catch (error) {
-        return { status: "error", message: error.message };
-      }
-    });
-  }
+      return {
+        status: "success",
+        message: `Message sent to ${nomor} successfully`,
+      };
+    } catch (error) {
+      return { status: "error", message: error.message };
+    }
+  });
 };
 
 export const addUrlsToQueue = async (messages) => {
@@ -64,10 +63,11 @@ export const addUrlsToQueue = async (messages) => {
     const { nomor, text } = msg;
     const encodedText = encodeURIComponent(text);
     const url = `https://web.whatsapp.com/send?phone=${nomor}&text=${encodedText}`;
+
     const result = await cluster.queue(url);
-    results.push(result);
+
+    results.push({ nomor, result });
   }
-  console.log(results);
   await cluster.idle();
   await cluster.close();
 
