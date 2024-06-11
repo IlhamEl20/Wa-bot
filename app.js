1a9bf8da71f90d70969572a871ab6f07132cdf99
Ilham <muhammadilhamsjn@gmail.com>
2024-06-11 14:19:14 +0700

    dockerfile and npmStart.sh added

--- app.js
+++ app.js
@@ -10,15 +10,14 @@ import swaggerUi from "swagger-ui-express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-// const running =
-//   process.env.RUNNING === "1" ? initializePuppeteer : initializeCluster();
-// app.set("trust proxy", true);
+const running =
+  process.env.RUNNING === "1" ? initializePuppeteer : initializeCluster();
+app.set("trust proxy", true);
 
 app.set("trust proxy", [
   "loopback",
   "linklocal",
   "uniquelocal",
-  "wa.frhan.site",
   "192.168.0.255",
 ]);
 // limit all request
@@ -32,17 +31,17 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 //one send one
-initializePuppeteer();
+// initializePuppeteer();
 
 // //broadcast
 // initializeCluster();
-// (async () => {
-//   try {
-//     await running();
-//   } catch (error) {
-//     console.error("Error executing the function:", error);
-//   }
-// })();
+(async () => {
+  try {
+    await running();
+  } catch (error) {
+    console.error("Error executing the function:", error);
+  }
+})();
 // Route to send message
 app.use("/", apiRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
